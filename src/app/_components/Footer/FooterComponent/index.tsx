db1d76d5aba1ac2997613919054d28e514509235
Hvitrevs <venusof12pm@proton.me>
2023-12-20 13:38:20 -0500

    cards for icons

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 
 import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 
 import classes from './index.module.scss'
-import Image from 'next/image'
 
 const FooterComponent = () => {
   const pathname = usePathname()
@@ -16,7 +16,7 @@ const FooterComponent = () => {
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
         <ul className={classes.inslussions}>
-          {inclusions.map((inclusion, index) => (
+          {inclusions.map(inclusion => (
             <li key={inclusion.title}>
               <Image
                 src={inclusion.icon}
@@ -25,6 +25,8 @@ const FooterComponent = () => {
                 height={36}
                 className={classes.icon}
               />
+              <h6 className={classes.title}>{inclusion.title}</h6>
+              <p>{inclusion.description}</p>
             </li>
           ))}
         </ul>
