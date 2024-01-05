44b0ce16e35176f57712da1b1e0a454e883846bf
Hvitrevs <venusof12pm@proton.me>
2024-01-05 16:49:55 -0500

    filters

--- src/app/(pages)/products/Filter/index.tsx
+++ src/app/(pages)/products/Filter/index.tsx
@@ -1,8 +1,48 @@
+'use client'
+
 import React from 'react'
 
-const Filters = () => {
+import { Category } from '../../../../payload/payload-types'
+// import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+// import { RadioButton } from '../../../_components/Radio'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {
+    if (categoryFilters.includes(categoryId)) {
+      const updatedCategories = categoryFilters.filter(id => id !== categoryId)
+
+      setCategoryFilters(updatedCategories)
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId])
+    }
+  }
+
+  const handleSort = (value: string) => setSort(value)
+
   return (
-    <div>Filters</div>
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return <h1>s</h1>
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By</h6>
+        <div className={classes.categories}>
+          <h1>s</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
